ddc051f1b238930d3f77c8f07e0745238612a0e4
顾建明 <jianming.gu@ele.me>
2017-10-18 12:49:47 +0800

    add find unused selector command

--- lib/objcthin.rb
+++ lib/objcthin.rb
@@ -26,7 +26,20 @@ module Imp
   class Objc
     def self.find_unused_sel(path)
       check_file_type(path)
-      find_impl_methods(path)
+      all_sels = find_impl_methods(path)
+      used_sel = reference_selectors(path)
+
+      unused_sel = []
+
+      all_sels.each do |sel,class_and_sels|
+        unless used_sel.include?(sel)
+          unused_sel += class_and_sels
+        end
+      end
+
+      puts Rainbow('below selector is unused:\n').red
+
+      puts unused_sel
     end
 
     def self.find_unused_class(path)
@@ -50,17 +63,98 @@ module Imp
     end
 
     def self.find_impl_methods(path)
+
+      app = %w[1 2]
+
+      apple_protocols = [
+          'tableView:canEditRowAtIndexPath:',
+      'commitEditingStyle:forRowAtIndexPath:',
+      'tableView:viewForHeaderInSection:',
+      'tableView:cellForRowAtIndexPath:',
+      'tableView:canPerformAction:forRowAtIndexPath:withSender:',
+      'tableView:performAction:forRowAtIndexPath:withSender:',
+      'tableView:accessoryButtonTappedForRowWithIndexPath:',
+      'tableView:willDisplayCell:forRowAtIndexPath:',
+          'tableView:commitEditingStyle:forRowAtIndexPath:',
+          'tableView:didEndDisplayingCell:forRowAtIndexPath:',
+          'tableView:didEndDisplayingHeaderView:forSection:',
+          'tableView:heightForFooterInSection:',
+          'tableView:shouldHighlightRowAtIndexPath:',
+          'tableView:shouldShowMenuForRowAtIndexPath:',
+          'tableView:viewForFooterInSection:',
+          'tableView:willDisplayHeaderView:forSection:',
+          'tableView:willSelectRowAtIndexPath:',
+          'willMoveToSuperview:',
+      'numberOfSectionsInTableView:',
+      'actionSheet:willDismissWithButtonIndex:',
+      'gestureRecognizer:shouldReceiveTouch:',
+      'gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:',
+      'gestureRecognizer:shouldReceiveTouch:',
+      'imagePickerController:didFinishPickingMediaWithInfo:',
+      'imagePickerControllerDidCancel:',
+      'animateTransition:',
+      'animationControllerForDismissedController:',
+      'animationControllerForPresentedController:presentingController:sourceController:',
+      'navigationController:animationControllerForOperation:fromViewController:toViewController:',
+      'navigationController:interactionControllerForAnimationController:',
+      'alertView:didDismissWithButtonIndex:',
+      'URLSession:didBecomeInvalidWithError:',
+      'setDownloadTaskDidResumeBlock:',
+      'tabBarController:didSelectViewController:',
+      'tabBarController:shouldSelectViewController:',
+      'applicationDidReceiveMemoryWarning:',
+      'application:didRegisterForRemoteNotificationsWithDeviceToken:',
+      'application:didFailToRegisterForRemoteNotificationsWithError:',
+      'application:didReceiveRemoteNotification:fetchCompletionHandler:',
+      'application:didRegisterUserNotificationSettings:',
+      'application:performActionForShortcutItem:completionHandler:',
+      'application:continueUserActivity:restorationHandler:'].freeze
+
       # imp -[class sel]
-      patten = /\s*imp\s*([+|-]\[.+\s(.+)\])/
+      sub_patten = /[+|-]\[.+\s(.+)\]/
+      patten = /\s*imp\s*(#{sub_patten})/
+      sel_set_patten = /set[A-Z].*:$/
+      sel_get_patten = /is[A-Z].*/
 
       output = `/usr/bin/otool -oV #{path}`
 
+      imp = {}
+
       output.each_line do |line|
         patten.match(line) do |m|
-          puts m[0], m[1]
+          sub = sub_patten.match(m[0]) do |subm|
+            class_and_sel = subm[0]
+            sel = subm[1]
+
+            next if sel.start_with?('.')
+            next if apple_protocols.include?(sel)
+            next if sel_set_patten.match?(sel)
+            next if sel_get_patten.match?(sel)
+
+            if imp.has_key?(sel)
+              imp[sel] << class_and_sel
+            else
+              imp[sel] = [class_and_sel]
+            end
+          end
         end
       end
 
+      imp.sort
+    end
+
+    def self.reference_selectors(path)
+      patten = /__TEXT:__objc_methname:(.+)/
+      output = `/usr/bin/otool -v -s __DATA __objc_selrefs #{path}`
+
+      sels = []
+      output.each_line do |line|
+        patten.match(line) do |m|
+          sels << m[1]
+        end
+      end
+
+      sels
     end
 
   end
